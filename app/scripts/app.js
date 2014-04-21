72d2a375d2a95a8a99d9b74cc8f453a27fa729e5
Swashcap <swashcap@gmail.com>
2014-04-20 17:17:36 -0700

    Provide initial data for app.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,40 +1,40 @@
 app = app || {};
 
 $(function () {
-  var beers = [
-    {
+  // Kick things off by creating the **App**
+  new app.AppView();
+
+  // Seed data if the nothing exists in localStorage
+  if ( ! localStorage.getItem(LOCAL_STORAGE_NS)) {
+    app.Beers.create({
       name: 'Amber Ale',
       brewery: 'Oakshire Brewing Co.',
       rating: 3,
       tags: ['amber', 'eugene']
-    },
-    {
+    });
+    app.Beers.create({
       name: 'Black Butte Porter',
       brewery: 'Deschutes Brewing',
       rating: 5,
       tags: ['porter']
-    },
-    {
+    });
+    app.Beers.create({
       name: 'Filament Winter IPA',
       brewery: 'Ecliptic Brewing',
       rating: 3,
       tags: ['ipa', 'portland']
-    },
-    {
+    });
+    app.Beers.create({
       name: 'Lompoc Special Draft',
       brewery: 'Lompoc Brewing Co.',
       rating: 3,
       tags: ['limited release', 'portland']
-    },
-    {
+    });
+    app.Beers.create({
       name: 'Hopportunity Knocks Ale',
       brewery: 'Caldera',
       rating: 2,
       tags: ['organic', 'ale']
-    }
-  ];
-
-  // Kick things off by creating the **App**
-  // new app.AppView(beers);
-  new app.AppView();
+    });
+  }
 });
